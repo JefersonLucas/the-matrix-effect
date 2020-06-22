021a75f5cb2c71cdca092197ca7b2ae020d65d05
Jeferson Lucas <kas_sants@hotmail.com>
2020-06-22 19:35:32 -0300

    Minified files #5

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,42 @@
+/*!
+ * Jeferson Luckas - the matrix effect
+ * 
+ * Copyright (c) 2020 Jeferson Luckas
+ * Released under the MIT license
+ * https://github.com/JefersonLucas/the-matrix-effect/blob/master/LICENSE
+ *
+ * Date: 2020-21-06
+ */
 
-//  Quando a página for carregada 
-//  Chama a função carregar()
-//
-window.onload = function carregar() {
-//  Captura o Canvas
+window.onload = function() {
+
     var canvas = document.getElementById("canvas");
-//  Passagem do canvas para a função efeitoMatrix()
-    efeitoMatrix(canvas);
-//  Função que efetua o efeito matrix
-    function efeitoMatrix(neo) {
-    //	Variáveis
-        var tela = window.screen;
-        var largura = (neo.width = tela.width);
-        var altura = (neo.height = tela.height);
-        var letras = Array(256).join(1).split("");
-    //	Desenha o efeito matrix na tela
-        var desenhaMatrix = function () {
-    //  Fundo Preto e transparência em .05
-        neo.getContext("2d").fillStyle = "rgba(0,0,0,.05)";
-        neo.getContext("2d").fillRect(0, 0, largura, altura);
-    //	Letras
-        neo.getContext("2d").fillStyle = "#0F0";
-    //  Função Map que desenha as letras
-        letras.map(function (posicao_y, index) {
-    //	Texto
-        var texto = String.fromCharCode(48 + Math.random() * 33);
-        var posicao_x = index * 10;
-        neo.getContext("2d").fillText(texto, posicao_x, posicao_y);
-    //	Array index de descreve o efeito matrix
-        letras[index] = posicao_y > 758 + Math.random() * 1e4 ? 0 : posicao_y + 10;
-    //
-        });
-      }
-    //  A um intervalo de milisegundos a função desenhaMatrix() é chamada
-        setInterval(desenhaMatrix, 60);
+
+    effectMatrix(canvas);
+
+    function effectMatrix(neo) {
+
+        var screen = window.screen;
+        var width = (neo.width = screen.width);
+        var height = (neo.height = screen.height);
+        var letters = Array(256).join(1).split("");
+
+        var designMatrix = function() {
+
+            neo.getContext("2d").fillStyle = "rgba(0,0,0,.05)";
+            neo.getContext("2d").fillRect(0, 0, width, height);
+
+            neo.getContext("2d").fillStyle = "#0F0";
+
+            letters.map(function (position_y, index) {
+
+                var text = String.fromCharCode(48 + Math.random() * 33);
+                var position_x = index * 10;
+                neo.getContext("2d").fillText(text, position_x, position_y);
+
+                letters[index] = position_y > 758 + Math.random() * 1e4 ? 0 : position_y + 10;
+            });        
+        }
+        setInterval(designMatrix, 60);
     }
 }
